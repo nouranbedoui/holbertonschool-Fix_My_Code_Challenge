316187b10c36c2ac3e9ebf25f17ffadc9acadb63
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-09-26 12:03:36 +0100

    print

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -4,9 +4,7 @@
 const arg = process.argv[2];
 const size = parseInt(arg, 10);
 
-if (isNaN(size) || size <= 0) {
-  console.log('');
-} else {
+if (!isNaN(size) && size > 0) {
   for (let i = 0; i < size; i++) {
     console.log('#'.repeat(size));
   }
