1428af0426cf484c05acb6b47261597e4d74f350
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-09-26 11:54:07 +0100

    print

---
+++ challenge/1-print_square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// prints a square of size 'size' with '#'
+
+const arg = process.argv[2];
+const size = parseInt(arg, 10);
+
+if (isNaN(size) || size <= 0) {
+  console.log('');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('#'.repeat(size));
+  }
+}
