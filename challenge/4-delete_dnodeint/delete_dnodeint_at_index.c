e25f7175eef56f660ac7860a4fb3f45239f3de7b
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-09-26 13:29:51 +0100

    delete

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -20,7 +20,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
     if (h->prev)
         h->prev->next = h->next;
     else
-        *head = h->next;  /* deleting head */
+        *head = h->next;
 
     if (h->next)
         h->next->prev = h->prev;
