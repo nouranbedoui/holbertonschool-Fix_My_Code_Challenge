0e31b206534cd4ac04e90c1a5ac72a26d9ade661
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-09-26 01:22:19 +0100

    delete

--- 4-delete_dnodeint/print_dlistint.c
+++ 4-delete_dnodeint/print_dlistint.c
@@ -1,23 +1,8 @@
-#include <stdio.h>
-#include "lists.h"
+#!/usr/bin/env node
+// Print a square of given size
 
-/**
- * print_dlistint - Prints a doubly linkedlist of integers
- *
- * @h: A pointer to the first element of a list
- *
- * Return: The number of element printed
- */
-size_t print_dlistint(const dlistint_t *h)
-{
-	size_t n;
+const size = parseInt(process.argv[2]);
 
-	n = 0;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		n++;
-	}
-	return (n);
+for (let i = 0; i < size; i++) {
+    console.log("#".repeat(size));
 }
