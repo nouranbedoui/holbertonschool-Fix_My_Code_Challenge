8d03fdbb21709dd26befa5e8bb6cbf3fead8d151
nouranbedoui <nuran.bedoui.000@gmail.com>
2025-09-26 12:09:37 +0100

    sort

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -8,3 +8,11 @@ ruby 2-sort.rb 12 41 2 C 9 -9 31 fun -1 32
 # 31
 # 32
 # 41
+#!/usr/bin/env ruby
+# Sort integer arguments in ascending order
+
+# Filter only valid integers
+numbers = ARGV.select { |arg| arg =~ /^-?\d+$/ }.map(&:to_i)
+
+# Sort numerically
+numbers.sort.each { |n| puts n }
